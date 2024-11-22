88a2839e75377472ae927e9c45562d7af6c81dbd
Carlos Arturo Araque Montoya <yaguarnecro@gmail.com>
2024-11-22 02:31:51 -0500

    succesfull set up of local env, next settingup dock env

--- GrowEbuddy_PSA/src/backend/api/serializers.py
+++
@@ -1,9 +0,0 @@
-# src/backend/api/serializers.py
-
-from rest_framework import serializers
-from .models import Vpet  # Import the Vpet model
-
-class VpetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vpet
-        fields = ['vpet_id', 'user_id', 'level', 'experience_points', 'mood']  # Adjust fields as necessary
